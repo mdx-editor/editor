5d5eb27c4d78763dff33d79cb0936debaae40212
Petyo Ivanov <underlog@gmail.com>
2023-02-15 12:48:23 +0200

    chore: ignore dist

--- packages/editor-ui/dist/index.cjs
+++
@@ -1,1 +0,0 @@
-"use strict";Object.defineProperty(exports,Symbol.toStringTag,{value:"Module"});const e=require("react");exports.LinkPopupPlugin=()=>e.createElement("div",null,"Hello World");
--- packages/editor-ui/dist/index.mjs
+++
@@ -1,7 +0,0 @@
-import React from "react";
-const LinkPopupPlugin = () => {
-  return /* @__PURE__ */ React.createElement("div", null, "Hello World");
-};
-export {
-  LinkPopupPlugin
-};
