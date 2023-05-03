63ab32e1db603f16a1d7c3dd2e10d616c499d891
Petyo Ivanov <underlog@gmail.com>
2023-05-03 10:47:01 +0300

    chore: silence eslint

--- src/ui/SourcePlugin/DiffViewer.tsx
+++ src/ui/SourcePlugin/DiffViewer.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react'
 import { diffLines, formatLines } from 'unidiff'
 import { parseDiff, Diff, Hunk } from 'react-diff-view'
