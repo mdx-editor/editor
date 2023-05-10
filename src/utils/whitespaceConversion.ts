df80e0a1406fe690f99d22e3893a68568a2bbc89
Petyo Ivanov <underlog@gmail.com>
2023-05-10 10:03:09 +0300

    chore: optimize whitespace padding

--- src/utils/whitespaceConversion.ts
+++ src/utils/whitespaceConversion.ts
@@ -1,15 +1,6 @@
 export const WHITESPACE_MARKER = '\u00A0'
 
+const WHITESPACE_REGEX = /^\s+|\s+$/g
 export function replaceWhitespace(str: string) {
-  // Replace whitespace at the beginning of the string
-  while (str.charAt(0) === ' ') {
-    str = WHITESPACE_MARKER + str.slice(1)
-  }
-
-  // Replace whitespace at the end of the string
-  while (str.charAt(str.length - 1) === ' ') {
-    str = str.slice(0, -1) + WHITESPACE_MARKER
-  }
-
-  return str
+  return str.replace(WHITESPACE_REGEX, (spaces) => WHITESPACE_MARKER.repeat(spaces.length))
 }
