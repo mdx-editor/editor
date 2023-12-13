aa684b70c1cff9026279a89944a5ce3ac6579863
Petyo Ivanov <underlog@gmail.com>
2023-12-13 13:35:29 +0200

    docs: catch all plugin example

--- src/examples/error-handling.tsx
+++ src/examples/error-handling.tsx
@@ -5,13 +5,18 @@ import {
   ConditionalContents,
   DiffSourceToggleWrapper,
   MDXEditor,
+  MdastImportVisitor,
   ShowSandpackInfo,
   UndoRedo,
   diffSourcePlugin,
+  realmPlugin,
+  system,
   toolbarPlugin
 } from '../'
 import markdown from './assets/buggy-markdown.md?raw'
 import { ALL_PLUGINS } from './_boilerplate'
+import * as Mdast from 'mdast'
+import { $createParagraphNode } from 'lexical'
 
 export function BuggyMarkdown() {
   return (
@@ -31,6 +36,57 @@ export function MissingPlugins() {
       markdown={`# Hello`}
       onChange={(md) => console.log('change', { md })}
       plugins={[
+        toolbarPlugin({
+          toolbarContents: () => (
+            <DiffSourceToggleWrapper>
+              <ConditionalContents
+                options={[
+                  { when: (editor) => editor?.editorType === 'codeblock', contents: () => <ChangeCodeMirrorLanguage /> },
+                  { when: (editor) => editor?.editorType === 'sandpack', contents: () => <ShowSandpackInfo /> },
+                  {
+                    fallback: () => (
+                      <>
+                        <UndoRedo />
+                        <BoldItalicUnderlineToggles />
+                      </>
+                    )
+                  }
+                ]}
+              />
+            </DiffSourceToggleWrapper>
+          )
+        }),
+        diffSourcePlugin({ viewMode: 'rich-text', diffMarkdown: 'boo' })
+      ]}
+    />
+  )
+}
+
+const CatchAllVisitor: MdastImportVisitor<Mdast.Content> = {
+  testNode: () => true,
+  visitNode: ({ mdastNode, actions }) => {
+    console.warn('catch all', { mdastNode })
+    actions.addAndStepInto($createParagraphNode())
+  },
+  priority: -500
+}
+
+const [catchAllPlugin] = realmPlugin({
+  id: 'catch-all',
+  systemSpec: system(() => ({})),
+  init: (realm) => {
+    realm.pubKey('addImportVisitor', CatchAllVisitor)
+  }
+})
+
+export function CatchAllPlugin() {
+  return (
+    <MDXEditor
+      onError={(msg) => console.warn(msg)}
+      markdown={`# Hello`}
+      onChange={(md) => console.log('change', { md })}
+      plugins={[
+        catchAllPlugin(),
         toolbarPlugin({
           toolbarContents: () => (
             <DiffSourceToggleWrapper>
