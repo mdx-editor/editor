0b49d74425f638b3afece2c96c457e5194ce88bd
Petyo Ivanov <underlog@gmail.com>
2024-01-06 14:31:05 +0200

    docs: mermaid code editor example

---
+++ src/examples/assets/mermaid-code.md
@@ -0,0 +1,12 @@
+Hello world
+
+```mmd
+graph TD
+    A[Enter Chart Definition] --> B(Preview)
+    B --> C{decide}
+    C --> D[Keep]
+    C --> E[Edit Definition]
+    E --> B
+    D --> F[Save Image and Code]
+    F --> B
+```
---
+++ src/examples/mermaid.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import mermaid from 'mermaid'
+import { CodeBlockEditorDescriptor, useCodeBlockEditorContext, MDXEditor, codeBlockPlugin } from '..'
+import mmdMarkdown from './assets/mermaid-code.md?raw'
+
+mermaid.initialize({ startOnLoad: true })
+
+const MermaidPreview: React.FC<{ code: string }> = ({ code }) => {
+  const ref = React.useRef<HTMLDivElement>(null)
+
+  React.useEffect(() => {
+    if (ref.current) {
+      void mermaid.render('graphDiv', code).then(({ svg }) => {
+        ref.current!.innerHTML = svg
+      })
+    }
+  }, [code])
+
+  return <div ref={ref}>{code}</div>
+}
+
+const MermaidCodeEditorDescriptor: CodeBlockEditorDescriptor = {
+  match: (language, _meta) => {
+    return language === 'mermaid' || language == 'mmd'
+  },
+  priority: 0,
+  Editor: (props) => {
+    const cb = useCodeBlockEditorContext()
+    const [code, setCode] = React.useState(props.code)
+
+    return (
+      <div onKeyDown={(e) => e.nativeEvent.stopImmediatePropagation()}>
+        <div style={{ display: 'flex' }}>
+          <textarea
+            style={{ flex: 1 }}
+            rows={3}
+            cols={20}
+            defaultValue={props.code}
+            onChange={(e) => {
+              setCode(e.target.value)
+              cb.setCode(e.target.value)
+            }}
+          />
+          <div style={{ flex: 1 }}>
+            <MermaidPreview code={code} />
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export function CodeBlock() {
+  return (
+    <MDXEditor
+      onChange={console.log}
+      markdown={mmdMarkdown}
+      plugins={[codeBlockPlugin({ codeBlockEditorDescriptors: [MermaidCodeEditorDescriptor] })]}
+    />
+  )
+}
