fbe4750302ff902deae6a8f7b6f077ed0a3062a1
Dharanish V <30527938+dharanish-v@users.noreply.github.com>
2024-04-30 13:13:20 +0530

    Remove postcss-extend plugin and update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
 import autoprefixer from 'autoprefixer'
 import nesting from 'postcss-nesting'
-import extend from 'postcss-extend'
 import mixins from 'postcss-mixins'
 
 export default {
-  plugins: [ autoprefixer, mixins, extend, nesting ]
+  plugins: [ autoprefixer, mixins, nesting ]
 }
