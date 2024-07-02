40a6be909bd7644a6810e1e7f0bebacf1f174d8d
Petyo Ivanov <underlog@gmail.com>
2024-07-02 09:28:48 +0300

    feat: upgrade to latest lexical
    Fixes #517

---
+++ scripts/upgrade-lexical.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+npm i --force @lexical/clipboard@latest \
+  @lexical/link@latest \
+  @lexical/list@latest \
+  @lexical/markdown@latest \
+  @lexical/plain-text@latest \
+  @lexical/react@latest \
+  @lexical/rich-text@latest \
+  @lexical/selection@latest \
+  @lexical/utils@latest \
+  lexical@latest
