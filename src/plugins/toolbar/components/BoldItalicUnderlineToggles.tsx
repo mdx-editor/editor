04c8c6c52932df856a1389008c3de05339cf9ec5
Basile Parent <casou.acc@gmail.com>
2024-07-23 20:04:00 +0200

    Refacto MultipleChoiceToggleGroup to match RadixUI specifications

--- src/plugins/toolbar/components/BoldItalicUnderlineToggles.tsx
+++ src/plugins/toolbar/components/BoldItalicUnderlineToggles.tsx
@@ -1,124 +1,144 @@
-import { applyFormat$, currentFormat$, iconComponentFor$, useTranslation } from '../../core'
-import { useCellValues, usePublisher } from '@mdxeditor/gurx'
-import React from 'react'
-import { FORMAT, IS_BOLD, IS_ITALIC, IS_STRIKETHROUGH, IS_SUBSCRIPT, IS_SUPERSCRIPT, IS_UNDERLINE } from '../../../FormatConstants'
-import { ToggleSingleGroupWithItem } from '.././primitives/toolbar'
-import { TextFormatType } from 'lexical'
+import {applyFormat$, currentFormat$, iconComponentFor$, useTranslation} from '../../core'
+import {useCellValues, usePublisher} from '@mdxeditor/gurx'
+import React, {useEffect, useState} from 'react'
+import {
+    FORMAT,
+    IS_BOLD,
+    IS_ITALIC,
+    IS_STRIKETHROUGH,
+    IS_SUBSCRIPT,
+    IS_SUPERSCRIPT,
+    IS_UNDERLINE
+} from '../../../FormatConstants'
+import {MultipleChoiceToggleGroup, ToggleSingleGroupWithItem} from '.././primitives/toolbar'
+import {TextFormatType} from 'lexical'
 import styles from '../../../styles/ui.module.css'
-import { IconKey } from '../../../defaultSvgIcons'
+import {IconKey} from '../../../defaultSvgIcons'
 
 interface FormatButtonProps {
-  format: FORMAT
-  addTitle: string
-  removeTitle: string
-  icon: IconKey
-  formatName: TextFormatType
+    format: FORMAT
+    addTitle: string
+    removeTitle: string
+    icon: IconKey
+    formatName: TextFormatType
 }
 
-const FormatButton: React.FC<FormatButtonProps> = ({ format, addTitle, removeTitle, icon, formatName }) => {
-  const [currentFormat, iconComponentFor] = useCellValues(currentFormat$, iconComponentFor$)
-  const applyFormat = usePublisher(applyFormat$)
-  const active = (currentFormat & format) !== 0
+const FormatButton: React.FC<FormatButtonProps> = ({format, addTitle, removeTitle, icon, formatName}) => {
+    const [currentFormat, iconComponentFor] = useCellValues(currentFormat$, iconComponentFor$)
+    const applyFormat = usePublisher(applyFormat$)
+    const active = (currentFormat & format) !== 0
 
-  return (
-    <ToggleSingleGroupWithItem
-      title={active ? removeTitle : addTitle}
-      on={active}
-      onValueChange={() => {
-        applyFormat(formatName)
-      }}
-    >
-      {iconComponentFor(icon)}
-    </ToggleSingleGroupWithItem>
-  )
+    return (
+        <ToggleSingleGroupWithItem
+            title={active ? removeTitle : addTitle}
+            on={active}
+            onValueChange={() => {
+                applyFormat(formatName)
+            }}
+        >
+            {iconComponentFor(icon)}
+        </ToggleSingleGroupWithItem>
+    )
 }
 
 export interface BoldItalicUnderlineTogglesProps {
-  options?: ('Bold' | 'Italic' | 'Underline')[]
+    options?: ('Bold' | 'Italic' | 'Underline')[]
 }
 
 /**
  * A toolbar component that lets the user toggle bold, italic and underline formatting.
  * @group Toolbar Components
  */
-export const BoldItalicUnderlineToggles: React.FC<BoldItalicUnderlineTogglesProps> = ({ options }) => {
-  const t = useTranslation()
+export const BoldItalicUnderlineToggles: React.FC<BoldItalicUnderlineTogglesProps> = ({options}) => {
+    const t = useTranslation()
+    const [currentFormat, iconComponentFor] = useCellValues(currentFormat$, iconComponentFor$)
+    const applyFormat = usePublisher(applyFormat$)
 
-  const showAllButtons = typeof options === 'undefined'
-  return (
-    <div className={styles.toolbarGroupOfGroups}>
-      {showAllButtons || options.includes('Bold') ? (
-        <FormatButton
-          format={IS_BOLD}
-          addTitle={t('toolbar.bold', 'Bold')}
-          removeTitle={t('toolbar.removeBold', 'Remove bold')}
-          icon="format_bold"
-          formatName="bold"
-        />
-      ) : null}
-      {showAllButtons || options.includes('Italic') ? (
-        <FormatButton
-          format={IS_ITALIC}
-          addTitle={t('toolbar.italic', 'Italic')}
-          removeTitle={t('toolbar.removeItalic', 'Remove italic')}
-          icon="format_italic"
-          formatName="italic"
-        />
-      ) : null}
-      {showAllButtons || options.includes('Underline') ? (
-        <FormatButton
-          format={IS_UNDERLINE}
-          addTitle={t('toolbar.underline', 'Underline')}
-          removeTitle={t('toolbar.removeUnderline', 'Remove underline')}
-          icon="format_underlined"
-          formatName="underline"
-        />
-      ) : null}
-    </div>
-  )
+    const showAllButtons = typeof options === 'undefined'
+    const isBold = (currentFormat & IS_BOLD) !== 0
+    const isItalic = (currentFormat & IS_ITALIC) !== 0
+    const isUnderline = (currentFormat & IS_UNDERLINE) !== 0
+
+    const [appliedFormats, setAppliedFormats] = useState<string[]>([])
+    useEffect(() => {
+        setAppliedFormats(
+            [
+                isBold ? 'bold' : null,
+                isItalic ? 'italic' : null,
+                isUnderline ? 'underline' : null
+            ].filter((f) => !!f) as TextFormatType[]
+        )
+    }, [currentFormat])
+    const handleApplyFormatDiff = (diff: string[]) => (diff as TextFormatType[]).forEach(applyFormat)
+
+    return (
+        <div className={styles.toolbarGroupOfGroups}>
+            <MultipleChoiceToggleGroup
+                value={appliedFormats}
+                onValueChange={setAppliedFormats}
+                onValueChangeDiff={handleApplyFormatDiff}
+                items={[
+                    ...(showAllButtons || options.includes('Bold') ? [{
+                        title: isBold ? t('toolbar.removeBold', 'Remove bold') : t('toolbar.bold', 'Bold'),
+                        contents: iconComponentFor('format_bold'),
+                        value: 'bold',
+                    }] : []),
+                    ...(showAllButtons || options.includes('Italic') ? [{
+                        title: isItalic ? t('toolbar.italic', 'Remove italic') : t('toolbar.italic', 'Italic'),
+                        contents: iconComponentFor('format_italic'),
+                        value: 'italic',
+                    }] : []),
+                    ...(showAllButtons || options.includes('Underline') ? [{
+                        title: isItalic ? t('toolbar.underline', 'Remove underline') : t('toolbar.underline', 'Underline'),
+                        contents: iconComponentFor('format_underlined'),
+                        value: 'underline',
+                    }] : []),
+                ]}/>
+        </div>
+    )
 }
 
 export interface StrikeThroughSupSubTogglesProps {
-  options?: ('Strikethrough' | 'Sub' | 'Sup')[]
+    options?: ('Strikethrough' | 'Sub' | 'Sup')[]
 }
 
 /**
  * A toolbar component that lets the user toggle strikeThrough, superscript and subscript formatting.
  * @group Toolbar Components
  */
-export const StrikeThroughSupSubToggles: React.FC<StrikeThroughSupSubTogglesProps> = ({ options }) => {
-  const t = useTranslation()
-  const showAllButtons = typeof options === 'undefined'
+export const StrikeThroughSupSubToggles: React.FC<StrikeThroughSupSubTogglesProps> = ({options}) => {
+    const t = useTranslation()
+    const showAllButtons = typeof options === 'undefined'
 
-  return (
-    <div className={styles.toolbarGroupOfGroups}>
-      {showAllButtons || options.includes('Strikethrough') ? (
-        <FormatButton
-          format={IS_STRIKETHROUGH}
-          addTitle={t('toolbar.strikethrough', 'Strikethrough')}
-          removeTitle={t('toolbar.removeStrikethrough', 'Remove strikethrough')}
-          icon="strikeThrough"
-          formatName="strikethrough"
-        />
-      ) : null}
-      {showAllButtons || options.includes('Sup') ? (
-        <FormatButton
-          format={IS_SUPERSCRIPT}
-          addTitle={t('toolbar.superscript', 'Superscript')}
-          removeTitle={t('toolbar.removeSuperscript', 'Remove superscript')}
-          icon="superscript"
-          formatName="superscript"
-        />
-      ) : null}
-      {showAllButtons || options.includes('Sub') ? (
-        <FormatButton
-          format={IS_SUBSCRIPT}
-          addTitle={t('toolbar.subscript', 'Subscript')}
-          removeTitle={t('toolbar.removeSubscript', 'Remove subscript')}
-          icon="subscript"
-          formatName="subscript"
-        />
-      ) : null}
-    </div>
-  )
+    return (
+        <div className={styles.toolbarGroupOfGroups}>
+            {showAllButtons || options.includes('Strikethrough') ? (
+                <FormatButton
+                    format={IS_STRIKETHROUGH}
+                    addTitle={t('toolbar.strikethrough', 'Strikethrough')}
+                    removeTitle={t('toolbar.removeStrikethrough', 'Remove strikethrough')}
+                    icon="strikeThrough"
+                    formatName="strikethrough"
+                />
+            ) : null}
+            {showAllButtons || options.includes('Sup') ? (
+                <FormatButton
+                    format={IS_SUPERSCRIPT}
+                    addTitle={t('toolbar.superscript', 'Superscript')}
+                    removeTitle={t('toolbar.removeSuperscript', 'Remove superscript')}
+                    icon="superscript"
+                    formatName="superscript"
+                />
+            ) : null}
+            {showAllButtons || options.includes('Sub') ? (
+                <FormatButton
+                    format={IS_SUBSCRIPT}
+                    addTitle={t('toolbar.subscript', 'Subscript')}
+                    removeTitle={t('toolbar.removeSubscript', 'Remove subscript')}
+                    icon="subscript"
+                    formatName="subscript"
+                />
+            ) : null}
+        </div>
+    )
 }
--- src/plugins/toolbar/components/CodeToggle.tsx
+++ src/plugins/toolbar/components/CodeToggle.tsx
@@ -1,26 +1,38 @@
-import React from 'react'
-import { IS_CODE } from '../../../FormatConstants'
-import { applyFormat$, currentFormat$, iconComponentFor$, useTranslation } from '../../core'
-import { MultipleChoiceToggleGroup } from '.././primitives/toolbar'
-import { useCellValues, usePublisher } from '@mdxeditor/gurx'
+import React, {useEffect, useState} from 'react'
+import {IS_CODE} from '../../../FormatConstants'
+import {applyFormat$, currentFormat$, iconComponentFor$, useTranslation} from '../../core'
+import {MultipleChoiceToggleGroup} from '.././primitives/toolbar'
+import {useCellValues, usePublisher} from '@mdxeditor/gurx'
+import {TextFormatType} from "lexical";
 
+1
 /**
  * A toolbar component that lets the user toggle code formatting.
  * Use for inline `code` elements (like variables, methods, etc).
  * @group Toolbar Components
  */
 export const CodeToggle: React.FC = () => {
-  const [currentFormat, iconComponentFor] = useCellValues(currentFormat$, iconComponentFor$)
-  const applyFormat = usePublisher(applyFormat$)
-  const t = useTranslation()
+    const [currentFormat, iconComponentFor] = useCellValues(currentFormat$, iconComponentFor$)
+    const applyFormat = usePublisher(applyFormat$)
+    const t = useTranslation()
 
-  const codeIsOn = (currentFormat & IS_CODE) !== 0
+    const codeIsOn = (currentFormat & IS_CODE) !== 0
 
-  const title = codeIsOn ? t('toolbar.removeInlineCode', 'Remove code format') : t('toolbar.inlineCode', 'Inline code format')
+    const title = codeIsOn ? t('toolbar.removeInlineCode', 'Remove code format') : t('toolbar.inlineCode', 'Inline code format')
 
-  return (
-    <MultipleChoiceToggleGroup
-      items={[{ title: title, contents: iconComponentFor('code'), active: codeIsOn, onChange: applyFormat.bind(null, 'code') }]}
-    />
-  )
+    const [appliedFormats, setAppliedFormats] = useState<string[]>([])
+    useEffect(() => {
+        setAppliedFormats([codeIsOn ? 'code' : null].filter((f) => !!f) as string[])
+    }, [currentFormat])
+
+    const handleApplyFormatDiff = (diff: string[]) => (diff as TextFormatType[]).forEach(applyFormat)
+
+    return (
+        <MultipleChoiceToggleGroup
+            value={appliedFormats}
+            onValueChange={setAppliedFormats}
+            onValueChangeDiff={handleApplyFormatDiff}
+            items={[{title: title, contents: iconComponentFor('code'), value: 'code'}]}
+        />
+    )
 }
