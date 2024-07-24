8c5c603bdb4a7b3e28df6c275c83aa7901f226fe
Basile Parent <casou.acc@gmail.com>
2024-07-24 14:55:59 +0200

    Formatting

--- src/plugins/toolbar/primitives/toolbar.tsx
+++ src/plugins/toolbar/primitives/toolbar.tsx
@@ -3,11 +3,11 @@ import * as RadixToolbar from '@radix-ui/react-toolbar'
 import classNames from 'classnames'
 import React from 'react'
 import styles from '../../../styles/ui.module.css'
-import {TooltipWrap} from './TooltipWrap'
-import {SelectButtonTrigger, SelectContent, SelectItem} from './select'
-import {EditorInFocus, editorInFocus$, readOnly$, useTranslation} from '../../core'
-import {useCellValue} from '@mdxeditor/gurx'
-import {TextFormatType} from "lexical";
+import { TooltipWrap } from './TooltipWrap'
+import { SelectButtonTrigger, SelectContent, SelectItem } from './select'
+import { EditorInFocus, editorInFocus$, readOnly$, useTranslation } from '../../core'
+import { useCellValue } from '@mdxeditor/gurx'
+import { TextFormatType } from 'lexical'
 
 //
 // function decorate<P extends { className?: string | undefined }>(Component: React.ComponentType<P>, decoratedProps: P) {
@@ -19,44 +19,44 @@ import {TextFormatType} from "lexical";
 //
 
 function decorateWithRef<P extends { className?: string | undefined }>(
-    Component: React.ForwardRefExoticComponent<P>,
-    decoratedProps: Partial<React.PropsWithoutRef<P>> & { 'data-toolbar-item'?: boolean }
+  Component: React.ForwardRefExoticComponent<P>,
+  decoratedProps: Partial<React.PropsWithoutRef<P>> & { 'data-toolbar-item'?: boolean }
 ) {
-    return React.forwardRef<object, P>((props: P, ref) => {
-        const className = classNames(decoratedProps.className, props.className)
-        return <Component {...decoratedProps} {...props} className={className} ref={ref}/>
-    })
+  return React.forwardRef<object, P>((props: P, ref) => {
+    const className = classNames(decoratedProps.className, props.className)
+    return <Component {...decoratedProps} {...props} className={className} ref={ref} />
+  })
 }
 
 function addTooltipToChildren<C extends React.ComponentType<{ children: React.ReactNode }>>(Component: C) {
-    return ({title, children, ...props}: React.ComponentProps<C> & { title: string }) => {
-        return (
-            <Component {...(props as any)}>
-                <TooltipWrap title={title}>{children}</TooltipWrap>
-            </Component>
-        )
-    }
+  return ({ title, children, ...props }: React.ComponentProps<C> & { title: string }) => {
+    return (
+      <Component {...(props as any)}>
+        <TooltipWrap title={title}>{children}</TooltipWrap>
+      </Component>
+    )
+  }
 }
 
 /**
  * @internal
  */
-export const Root: React.FC<{ readOnly: boolean; children: React.ReactNode }> = ({readOnly, children}) => {
-    return (
-        <RadixToolbar.Root
-            className={classNames('mdxeditor-toolbar', styles.toolbarRoot, {[styles.readOnlyToolbarRoot]: readOnly})}
-            {...(readOnly ? {tabIndex: -1} : {})}
-        >
-            {children}
-        </RadixToolbar.Root>
-    )
+export const Root: React.FC<{ readOnly: boolean; children: React.ReactNode }> = ({ readOnly, children }) => {
+  return (
+    <RadixToolbar.Root
+      className={classNames('mdxeditor-toolbar', styles.toolbarRoot, { [styles.readOnlyToolbarRoot]: readOnly })}
+      {...(readOnly ? { tabIndex: -1 } : {})}
+    >
+      {children}
+    </RadixToolbar.Root>
+  )
 }
 
 /**
  * A toolbar button primitive.
  * @group Toolbar Primitives
  */
-export const Button = decorateWithRef(RadixToolbar.Button, {className: styles.toolbarButton, 'data-toolbar-item': true})
+export const Button = decorateWithRef(RadixToolbar.Button, { className: styles.toolbarButton, 'data-toolbar-item': true })
 
 /**
  * A toolbar button with tooltip primitive.
@@ -68,38 +68,38 @@ export const ButtonWithTooltip = addTooltipToChildren(Button)
  * @internal
  */
 export const ToolbarToggleItem = decorateWithRef(RadixToolbar.ToggleItem, {
-    className: styles.toolbarToggleItem,
-    'data-toolbar-item': true
+  className: styles.toolbarToggleItem,
+  'data-toolbar-item': true
 })
 
 /**
  * @internal
  */
 export const SingleToggleGroup = decorateWithRef(RadixToolbar.ToggleGroup, {
-    type: 'single',
-    className: styles.toolbarToggleSingleGroup
+  type: 'single',
+  className: styles.toolbarToggleSingleGroup
 })
 
 /**
  * @internal
  */
 export const ToggleSingleGroupWithItem = React.forwardRef<
-    HTMLDivElement,
-    Omit<RadixToolbar.ToolbarToggleGroupSingleProps, 'type'> & { on: boolean; title: string; disabled?: boolean }
->(({on, title, children, disabled, ...props}, forwardedRef) => {
-    return (
-        <RadixToolbar.ToggleGroup
-            type="single"
-            className={styles.toolbarToggleSingleGroup}
-            {...props}
-            value={on ? 'on' : 'off'}
-            ref={forwardedRef}
-        >
-            <ToolbarToggleItem title={title} value="on" disabled={disabled}>
-                <TooltipWrap title={title}>{children}</TooltipWrap>
-            </ToolbarToggleItem>
-        </RadixToolbar.ToggleGroup>
-    )
+  HTMLDivElement,
+  Omit<RadixToolbar.ToolbarToggleGroupSingleProps, 'type'> & { on: boolean; title: string; disabled?: boolean }
+>(({ on, title, children, disabled, ...props }, forwardedRef) => {
+  return (
+    <RadixToolbar.ToggleGroup
+      type="single"
+      className={styles.toolbarToggleSingleGroup}
+      {...props}
+      value={on ? 'on' : 'off'}
+      ref={forwardedRef}
+    >
+      <ToolbarToggleItem title={title} value="on" disabled={disabled}>
+        <TooltipWrap title={title}>{children}</TooltipWrap>
+      </ToolbarToggleItem>
+    </RadixToolbar.ToggleGroup>
+  )
 })
 
 /**
@@ -107,95 +107,90 @@ export const ToggleSingleGroupWithItem = React.forwardRef<
  * @group Toolbar Primitives
  */
 export const MultipleChoiceToggleGroup: React.FC<
-    Omit<RadixToolbar.ToolbarToggleGroupMultipleProps, 'type'> & {
+  Omit<RadixToolbar.ToolbarToggleGroupMultipleProps, 'type'> & {
     onValueChangeDiff?: (diff: string[]) => void
     items: {
-        title: string
-        contents: React.ReactNode
-        value: string
-        disabled?: boolean
+      title: string
+      contents: React.ReactNode
+      value: string
+      disabled?: boolean
     }[]
-}> = ({items, value, onValueChange, onValueChangeDiff, ...otherProps}) => {
-
-    const handleValueChange = (newValue: string[]) => {
-        if (onValueChangeDiff) {
-            const diff = [
-                ...newValue.filter((format) => !(value || []).includes(format)),
-                ...(value || []).filter((existingFormat) => !newValue.includes(existingFormat)),
-            ]
-            onValueChangeDiff(diff)
-        }
-
-        onValueChange?.(newValue)
+  }
+> = ({ items, value, onValueChange, onValueChangeDiff, ...otherProps }) => {
+  const handleValueChange = (newValue: string[]) => {
+    if (onValueChangeDiff) {
+      const diff = [
+        ...newValue.filter((format) => !(value || []).includes(format)),
+        ...(value || []).filter((existingFormat) => !newValue.includes(existingFormat))
+      ]
+      onValueChangeDiff(diff)
     }
 
-    return (
-        <div className={styles.toolbarGroupOfGroups}>
-            <RadixToolbar.ToggleGroup
-                type="multiple"
-                className={styles.toolbarToggleSingleGroup}
-                value={value || []}
-                onValueChange={handleValueChange}
-                { ...otherProps }
-            >
-                {items.map((item, index) => (
-                    <ToolbarToggleItem title={item.title}
-                                       value={item.value}
-                                       disabled={item.disabled}
-                                       key={index}
-                    >
-                        <TooltipWrap title={item.title}>{item.contents}</TooltipWrap>
-                    </ToolbarToggleItem>
-                ))}
-            </RadixToolbar.ToggleGroup>
-        </div>
-    )
-}
+    onValueChange?.(newValue)
+  }
 
+  return (
+    <div className={styles.toolbarGroupOfGroups}>
+      <RadixToolbar.ToggleGroup
+        type="multiple"
+        className={styles.toolbarToggleSingleGroup}
+        value={value || []}
+        onValueChange={handleValueChange}
+        {...otherProps}
+      >
+        {items.map((item, index) => (
+          <ToolbarToggleItem title={item.title} value={item.value} disabled={item.disabled} key={index}>
+            <TooltipWrap title={item.title}>{item.contents}</TooltipWrap>
+          </ToolbarToggleItem>
+        ))}
+      </RadixToolbar.ToggleGroup>
+    </div>
+  )
+}
 
 /**
  * A toolbar primitive that allows you to build an UI with multiple exclusive toggle groups, like the list type toggle.
  * @group Toolbar Primitives
  */
 export const SingleChoiceToggleGroup = <T extends string>({
-                                                              value,
-                                                              onChange,
-                                                              className,
-                                                              "aria-label": ariaLabel,
-                                                              items
-                                                          }: {
-    items: {
-        title: string
-        value: T
-        contents: React.ReactNode
-    }[]
-    "aria-label": string
-    onChange: (value: T | '') => void
-    value: T | ''
-    className?: string
+  value,
+  onChange,
+  className,
+  'aria-label': ariaLabel,
+  items
+}: {
+  items: {
+    title: string
+    value: T
+    contents: React.ReactNode
+  }[]
+  'aria-label': string
+  onChange: (value: T | '') => void
+  value: T | ''
+  className?: string
 }) => {
-    const t = useTranslation()
+  const t = useTranslation()
 
-    return (
-        <div className={styles.toolbarGroupOfGroups}>
-            <RadixToolbar.ToggleGroup
-                aria-label={ariaLabel}
-                type="single"
-                className={classNames(styles.toolbarToggleSingleGroup, className)}
-                onValueChange={onChange}
-                value={value || ''}
-                onFocus={(e) => {
-                    e.preventDefault()
-                }}
-            >
-                {items.map((item, index) => (
-                    <ToolbarToggleItem key={index} aria-label={item.title} value={item.value}>
-                        <TooltipWrap title={item.title}>{item.contents}</TooltipWrap>
-                    </ToolbarToggleItem>
-                ))}
-            </RadixToolbar.ToggleGroup>
-        </div>
-    )
+  return (
+    <div className={styles.toolbarGroupOfGroups}>
+      <RadixToolbar.ToggleGroup
+        aria-label={ariaLabel}
+        type="single"
+        className={classNames(styles.toolbarToggleSingleGroup, className)}
+        onValueChange={onChange}
+        value={value || ''}
+        onFocus={(e) => {
+          e.preventDefault()
+        }}
+      >
+        {items.map((item, index) => (
+          <ToolbarToggleItem key={index} aria-label={item.title} value={item.value}>
+            <TooltipWrap title={item.title}>{item.contents}</TooltipWrap>
+          </ToolbarToggleItem>
+        ))}
+      </RadixToolbar.ToggleGroup>
+    </div>
+  )
 }
 
 /**
@@ -203,61 +198,61 @@ export const SingleChoiceToggleGroup = <T extends string>({
  * @group Toolbar Primitives
  */
 export const ButtonOrDropdownButton = <T extends string>(props: {
+  /**
+   * The contents of the button - usually an icon.
+   */
+  children: React.ReactNode
+  /**
+   * The title used for the tooltip.
+   */
+  title: string
+  /**
+   * The function to execute when the button is clicked or an item is chosen from the dropdown.
+   * If there is only one item in the dropdown, the value will be an empty string.
+   */
+  onChoose: (value: T) => void
+  /**
+   * The items to show in the dropdown.
+   */
+  items: {
     /**
-     * The contents of the button - usually an icon.
+     * The value to pass to the `onChoose` function when this item is chosen.
      */
-    children: React.ReactNode
+    value: T
     /**
-     * The title used for the tooltip.
+     * The label to show in the dropdown.
      */
-    title: string
-    /**
-     * The function to execute when the button is clicked or an item is chosen from the dropdown.
-     * If there is only one item in the dropdown, the value will be an empty string.
-     */
-    onChoose: (value: T) => void
-    /**
-     * The items to show in the dropdown.
-     */
-    items: {
-        /**
-         * The value to pass to the `onChoose` function when this item is chosen.
-         */
-        value: T
-        /**
-         * The label to show in the dropdown.
-         */
-        label: string | JSX.Element
-    }[]
+    label: string | JSX.Element
+  }[]
 }) => {
-    const readOnly = useCellValue(readOnly$)
-    return (
-        <>
-            {props.items.length === 1 ? (
-                <ButtonWithTooltip
-                    title={props.title}
-                    onClick={() => {
-                        props.onChoose('' as T)
-                    }}
-                    disabled={readOnly}
-                >
-                    {props.children}
-                </ButtonWithTooltip>
-            ) : (
-                <RadixSelect.Root value="" onValueChange={props.onChoose}>
-                    <SelectButtonTrigger title={props.title}>{props.children}</SelectButtonTrigger>
+  const readOnly = useCellValue(readOnly$)
+  return (
+    <>
+      {props.items.length === 1 ? (
+        <ButtonWithTooltip
+          title={props.title}
+          onClick={() => {
+            props.onChoose('' as T)
+          }}
+          disabled={readOnly}
+        >
+          {props.children}
+        </ButtonWithTooltip>
+      ) : (
+        <RadixSelect.Root value="" onValueChange={props.onChoose}>
+          <SelectButtonTrigger title={props.title}>{props.children}</SelectButtonTrigger>
 
-                    <SelectContent className={styles.toolbarButtonDropdownContainer}>
-                        {props.items.map((item, index) => (
-                            <SelectItem key={index} value={item.value}>
-                                {item.label}
-                            </SelectItem>
-                        ))}
-                    </SelectContent>
-                </RadixSelect.Root>
-            )}
-        </>
-    )
+          <SelectContent className={styles.toolbarButtonDropdownContainer}>
+            {props.items.map((item, index) => (
+              <SelectItem key={index} value={item.value}>
+                {item.label}
+              </SelectItem>
+            ))}
+          </SelectContent>
+        </RadixSelect.Root>
+      )}
+    </>
+  )
 }
 
 /**
@@ -265,14 +260,14 @@ export const ButtonOrDropdownButton = <T extends string>(props: {
  * @group Toolbar Primitives
  */
 export interface ConditionalContentsOption {
-    /**
-     * A function that returns `true` if the option should be displayed for the current editor in focus.
-     */
-    when: (rootNode: EditorInFocus | null) => boolean
-    /**
-     * The contents to display if the `when` function returns `true`.
-     */
-    contents: () => React.ReactNode
+  /**
+   * A function that returns `true` if the option should be displayed for the current editor in focus.
+   */
+  when: (rootNode: EditorInFocus | null) => boolean
+  /**
+   * The contents to display if the `when` function returns `true`.
+   */
+  contents: () => React.ReactNode
 }
 
 /**
@@ -280,14 +275,14 @@ export interface ConditionalContentsOption {
  * @group Toolbar Primitives
  */
 export interface FallbackOption {
-    /**
-     * The contents to display
-     */
-    fallback: () => React.ReactNode
+  /**
+   * The contents to display
+   */
+  fallback: () => React.ReactNode
 }
 
 function isConditionalContentsOption(option: ConditionalContentsOption | FallbackOption): option is ConditionalContentsOption {
-    return Object.hasOwn(option, 'when')
+  return Object.hasOwn(option, 'when')
 }
 
 /**
@@ -314,28 +309,28 @@ function isConditionalContentsOption(option: ConditionalContentsOption | Fallbac
  * @group Toolbar Primitives
  */
 export const ConditionalContents: React.FC<{
-    /**
-     * A set of options that define the contents to show based on the editor that is in focus.
-     * Can be either a {@link ConditionalContentsOption} or a {@link FallbackOption}.
-     * See the {@link ConditionalContents} documentation for an example.
-     */
-    options: (ConditionalContentsOption | FallbackOption)[]
-}> = ({options}) => {
-    const editorInFocus = useCellValue(editorInFocus$)
-    const contents = React.useMemo(() => {
-        const option = options.find((option) => {
-            if (isConditionalContentsOption(option)) {
-                if (option.when(editorInFocus)) {
-                    return true
-                }
-            } else {
-                return true
-            }
-        })
-        return option ? (isConditionalContentsOption(option) ? option.contents() : option.fallback()) : null
-    }, [options, editorInFocus])
+  /**
+   * A set of options that define the contents to show based on the editor that is in focus.
+   * Can be either a {@link ConditionalContentsOption} or a {@link FallbackOption}.
+   * See the {@link ConditionalContents} documentation for an example.
+   */
+  options: (ConditionalContentsOption | FallbackOption)[]
+}> = ({ options }) => {
+  const editorInFocus = useCellValue(editorInFocus$)
+  const contents = React.useMemo(() => {
+    const option = options.find((option) => {
+      if (isConditionalContentsOption(option)) {
+        if (option.when(editorInFocus)) {
+          return true
+        }
+      } else {
+        return true
+      }
+    })
+    return option ? (isConditionalContentsOption(option) ? option.contents() : option.fallback()) : null
+  }, [options, editorInFocus])
 
-    return <div style={{display: 'flex'}}>{contents}</div>
+  return <div style={{ display: 'flex' }}>{contents}</div>
 }
 
 /**
