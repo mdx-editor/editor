c7a328f0f08e051360a99fef7a1d8d66f65c8471
Moritz Tim W. <90388353+moritztim@users.noreply.github.com>
2024-11-30 17:28:52 +0100

    Add defaults

--- src/plugins/table/index.ts
+++ src/plugins/table/index.ts
@@ -89,7 +89,7 @@ export const tablePlugin = realmPlugin<GfmTableOptions>({
       // export
       [addLexicalNode$]: TableNode,
       [addExportVisitor$]: LexicalTableVisitor,
-      [addToMarkdownExtension$]: gfmTableToMarkdown({ tableCellPadding: params?.tableCellPadding, tablePipeAlign: params?.tablePipeAlign })
+      [addToMarkdownExtension$]: gfmTableToMarkdown({ tableCellPadding: params?.tableCellPadding ?? true, tablePipeAlign: params?.tablePipeAlign ?? true })
     })
   }
 })
